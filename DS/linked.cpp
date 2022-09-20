9c9b01b6ae2830298d32f98d46d4319a8724d443
Mohak Khetan <92690177+DatMomoAgain@users.noreply.github.com>
2022-09-20 11:51:02 +0530

    Update linked.cpp

--- DS/linked.cpp
+++ DS/linked.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-
 using namespace std;
+
 class Node
 {
 public:
@@ -19,14 +19,16 @@ void traverse(Node* n)
 
 Node* insert(int x, Node* head)
 {
-    cout<<"1-begining 2-end: ";
+    cout<<"1-begining 2-end 3-middle: ";
     int q;
     cin>>q;
     Node* n=NULL;
     n = new Node();
     n->data = x;
     Node* temp = NULL;
+    Node* temp2 = NULL;
     temp = head;
+    temp2 = head;
     switch(q)
     {
      case 1:
@@ -39,7 +41,27 @@ Node* insert(int x, Node* head)
         {
             temp = temp->next;
         }
-        
+        temp->next = n;              
+        break;
+    
+     case 3:
+        cout << "Enter 1st node: ";
+        int n1;
+        cin >> n1;
+        cout << "Enter 2nd node: ";
+        int n2;
+        cin >> n2;
+
+        for(int i=0; i<n1-1; i++)
+        {
+            temp = temp->next;
+        }
+        for(int i=0; i<n2-1; i++)
+        {
+            temp2 = temp2->next;
+        }
+        n->next = temp2;
+        temp->next = n;
         break;
      default:
         break;
@@ -62,6 +84,8 @@ int main()
     sec->next = NULL;
     first->next = sec;
     head = insert(3,head);
+    head =insert(4,head);
+    head =insert(5,head);
     traverse(head);
 
     return 0;
