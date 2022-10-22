3a641c52481f402aee67d731cefc5c4a001ee662
Mohak Khetan <92690177+DatMomoAgain@users.noreply.github.com>
2022-10-22 13:04:00 +0530

    Update linked.cpp

--- DS/linked.cpp
+++ DS/linked.cpp
@@ -8,158 +8,117 @@ public:
     Node* next;
 };
 
-void traverse(Node* n)
+void traverse(Node* head)
 {
+    if(head == NULL)
+    {
+        cout<<"List is empty";
+    }
+    Node *n = head;
     while (n != NULL)
     {
-       cout<< n->data << ' '; 
-       n = n->next;
+        
+        cout<< n->data << ' '; 
+        n = n->next;
     }
     cout<<'\n';
 }
 
 Node* insert(int x, Node* head)
 {
-    cout<<"1-begining 2-end 3-middle: ";
-    int q;
-    cin>>q;
+
+    cout<<"Enter index: ";
+    int q1;
+    cin>>q1;
     Node* n=NULL;
     n = new Node();
     n->data = x;
-    Node* temp = NULL;
-    Node* temp2 = NULL;
-    temp = head;
-    temp2 = head;
-    switch(q)
+    Node* temp = head;
+    if(q1 == 0)
     {
-     case 1:
         n->next = head;
         head = n;
-        break;
-     case 2:
-        n->next = NULL;
-        while(temp->next != NULL)
+    }
+    else
+    {
+        for(int i=0; i<q1; i++)
         {
-            temp = temp->next;
+            //temp points to prev node
+            if(i!=0){
+            temp = temp->next;}
+            if(temp == NULL)
+            {
+                cout<<"Invalid index\n";
+                return head;
+            }            
         }
-        temp->next = n;              
-        break;
-    
-     case 3:
-        cout << "Enter 1st node: ";
-        int n1;
-        cin >> n1;
-        cout << "Enter 2nd node: ";
-        int n2;
-        cin >> n2;
-
-        for(int i=0; i<n1-1; i++)
-        {
-            temp = temp->next;
-        }
-        for(int i=0; i<n2-1; i++)
-        {
-            temp2 = temp2->next;
-        }
-        n->next = temp2;
+        n->next = temp->next;
         temp->next = n;
-        break;
-     default:
-        break;
     }
     return head;
 }
 
 Node* eradicate(Node* head)
 {
-    cout<<"1-begining 2-end 3-middle: ";
+    if(head ==NULL)
+    {
+        cout<<"List is empty\n";
+        return head;
+    }
+    cout<<"Enter index: ";
     int q;
     cin>>q;
     Node* temp = head;
     Node* temp2 = head;
-    switch(q)
+    if(q==0)
     {
-     case 1:
-        temp = temp->next;
-        head = temp;
-        break;
-     case 2:
-        while(temp->next != NULL)
-        {
-            temp = temp->next;
-        }
-        while(temp2->next != temp)
-        {
-            temp2 = temp2->next;
-        }
-        temp2->next = NULL;
-        break;
-     case 3:
-        cout << "Enter node you want to delete: ";
-        int n1;
-        cin >> n1;
-
-        for(int i=0; i<n1-2; i++)
-        {
-            temp = temp->next;
-        }
-        for(int i=0; i<n1; i++)
-        {
-            temp2 = temp2->next;
-        }
-        temp->next = temp2;
-        break;
+        head = head->next;
+        delete(temp);
+        return head;
     }
+    for(int i=0; i<q;i++)
+    {
+        if(i!=0)
+        {
+            temp = temp->next;
+        }
+        if(temp->next==NULL)
+        {
+            cout<<"Invalid index\n";
+        }
+        temp2 = temp2->next;
+    }
+    temp->next= temp->next->next;
+    delete(temp2);
     return head;
 }
 
-Node* sort(Node* head)
-{
-    Node* temp1 = head->next;
-    Node* max = head;
-    
-    while(temp1 != NULL)
-    {
-        if(max->data<temp1->data)
-        {
-            max = temp1;
-        }
-        temp1 = temp1->next;
-    }
-    if (max->next != NULL)
-    {
-        
-    }
-    
-    
-    
-}
+
 
 int main()
 {
     Node* head = NULL;
-    Node* first = NULL;
-    first = new Node();
-    first->data = 1;
-    first->next = NULL;
-    head = first;
-    Node* sec = NULL;
-    sec = new Node();
-    sec->data=2;
-    sec->next = NULL;
-    first->next = sec;
-    head = insert(3,head);
-    traverse(head);
-    head =insert(4,head);
-    traverse(head);
-    head =insert(5,head);
-    traverse(head);
-    head = eradicate(head);
-    traverse(head);
-    head = eradicate(head);
-    traverse(head);
-    head = eradicate(head);
-    traverse(head);
+    int q3 = -1;
+    while(q3!=0)
+    {
+        cout<<"Enter 1-insert; 2-delete; 3-traverse; 0-exit: ";
+        cin>>q3;
+        switch(q3){
+         case 0:
+            break;
+         case 1:
+            cout<<"Enter no: ";
+            int n1;
+            cin>>n1;
+            head = insert(n1, head);
+            break;
+         case 2:
+            head = eradicate(head);
+            break;
+         case 3:
+            traverse(head);            
+        }
+    }
 
     return 0;
 }
