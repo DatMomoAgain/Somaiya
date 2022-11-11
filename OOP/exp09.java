2c6330d06583f36d646ace6d9ee50afade1bd93d
Mohak Khetan <92690177+DatMomoAgain@users.noreply.github.com>
2022-11-11 12:36:14 +0530

    Create exp09.java

---
+++ OOP/exp09.java
@@ -0,0 +1,11 @@
+import engineering.*;
+import java.util.*;
+
+public class Exp09 {
+    public static  void main(String args[])
+    {
+        Vector<Student> v = Student.create();
+        Marks obj = new Marks();
+        obj.display(v);
+    }
+}
