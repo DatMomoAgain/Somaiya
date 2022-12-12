e86c6f9883a4f7a6c24d8f2ad5d82d509c6d2170
Mohak Khetan <92690177+DatMomoAgain@users.noreply.github.com>
2022-12-13 01:59:47 +0530

    Update and rename graph(BFS-only).cpp to graph.cpp

--- DS/graph.cpp
+++ DS/graph.cpp
@@ -2,22 +2,40 @@
 #include<unordered_map>
 #include<vector>
 #include<queue>
+#include <stack>
 using namespace std;
 
-/*void dfs(int index,bool visited[],int** graph, int vertices)
+void dfs(int index,int** graph, int vertices, int starting)
 {
-
-    visited[index]=true;
-    cout<<index<<" ";
-    for(auto i:graph[index])
+    int marked[vertices];
+    for(int i=0; i<vertices; i++)
     {
-        
-        if(!visited[i])
-        {
-            dfs(i,visited,graph);
-        }   
+        marked[i]=0;
     }
-}*/
+    marked[starting] =1;
+    stack<int> stack;
+    stack.push(starting);
+
+    while(!stack.empty())
+    {
+        int temp = stack.top();
+        stack.pop();
+        cout<<temp<<' ';
+
+        for(int i=vertices-1; i>=0; i--)
+        {
+            if(graph[temp][i] ==1)
+            {
+                if(marked[i]==0)
+                {
+                    marked[i]=1;
+                    stack.push(i);
+                }
+            }
+        }
+    }
+    cout<<endl;
+}
 
 void bfs(int index,bool visited[],int** graph, int vertices)
 {
@@ -66,13 +84,6 @@ int main()
         }
     }
 
-    for(int i=0; i<vertices; i++)
-    {
-        for(int j=0; j<vertices; j++)
-        {
-            graph[i][j]=0;
-        }
-    }
     for (int i = 0; i < vertices; i++)
     {
         cout<<"Enter no. of edges for vertice "<<i<<": ";
@@ -84,7 +95,7 @@ int main()
             int v2;
             cout<<"Enter vertex edge connects to: ";
             cin>>v2;
-            graph[v1][v2] = 1;            
+            graph[v1][v2] = 1;
         }
     }
 
@@ -94,8 +105,11 @@ int main()
         visited[i]=false;
     }
 
-    //cout<<"\nDFS Traversal:\n";
-    //dfs(0,visited,graph);
+    cout<<"\nDFS Traversal:\n";
+    cout<<"Enter starting vertex: ";
+    int t2;
+    cin>>t2;
+    dfs(0,graph,vertices, t2);
 
     for(int i=0;i<vertices;i++)
     {
